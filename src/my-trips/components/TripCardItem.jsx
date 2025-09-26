3ebcb9bc9ed26895a17ba458aa6591a3e7ec4712
RabbitBoii <chetanatram11284@gmail.com>
2025-09-26 21:59:50 +0530

    extracted user trips from db and saved them

---
+++ src/my-trips/components/TripCardItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TripCardItem({ trip }) {
+    return (
+        <div>TripCardItem</div>
+    )
+}
+
+export default TripCardItem
--- src/my-trips/index.jsx
+++ src/my-trips/index.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import { db } from '@/service/FirebaseConfig'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import TripCardItem from './components/TripCardItem'
 
 function MyTrips() {
-    const user = JSON.parse(localStorage.getItem("user"));
+    const navigate = useNavigate()
+    const [userTrips, setUserTrips] = useState([]);
+
+
+    useEffect(() => {
+        GetUserTrips()
+    }, [])
+
+
+    const GetUserTrips = async () => {
+
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        if (!user) {
+            navigate("/")
+            return;
+        }
+        setUserTrips([])
+
+        const qt = query(collection(db, "UserTrips"), where("userEmail", "==", user?.email));
+        const querySnapshot = await getDocs(qt);
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            setUserTrips(prevVal => [...prevVal, doc.data()])
+        });
+
+    }
 
 
     return (
-        <div>
-            <h2>My Trip</h2>
-            {/* <div>
-                <h2>{user.}</h2>
-            </div> */}
-
+        <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
+            <h2 className='font-bold text-3xl'>My Trips</h2>
+            <div>
+                {userTrips.map((trip, index) => (
+                    <TripCardItem trip={trip} />
+                ))}
+            </div>
         </div>
     )
 }
